60234f9705bbf4a5d4fcd6febf2b2199688c38ad
Montes-Tobar <mt17005@ues.edu.sv>
2022-06-03 22:46:59 -0600

    Agregada librería Calendar

--- app/src/main/java/com/example/proyecto2pdm115/Carrito.java
+++ app/src/main/java/com/example/proyecto2pdm115/Carrito.java
@@ -2,6 +2,7 @@ package com.example.proyecto2pdm115;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -24,8 +26,13 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import java.util.Calendar;
+
 
 public class Carrito extends Activity {
+
+    private TextView tvSelectDate;
+    private EditText etSelectDate;
 
     ListView listado;
     ImageButton clear;
@@ -37,6 +44,7 @@ public class Carrito extends Activity {
     int cc;
     String v = "1";
 
+
     private Socket socket;
     private static final int SERVERPORT = 6000;
     private static final String SERVER_IP = "192.168.0.101";
@@ -45,6 +53,58 @@ public class Carrito extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrilto);
+
+        //============================== Inicio Calendar ================================
+
+        tvSelectDate = findViewById(R.id.tvSelectDate);
+        etSelectDate = findViewById(R.id.etSelectDate);
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog dialog = new DatePickerDialog(Carrito.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        month = month+1;
+                        String date = dayOfMonth+"/"+month+"/"+year;
+                        tvSelectDate.setText(date);
+
+                    }
+                },year, month,day);
+                dialog.show();
+
+            }
+        });
+
+        etSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog dialog = new DatePickerDialog(Carrito.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        month = month+1;
+                        String date = dayOfMonth+"/"+month+"/"+year;
+                        etSelectDate.setText(date);
+
+                    }
+                },year, month,day);
+                dialog.show();
+
+            }
+        });
+
+        //============================== Fin Calendar ================================
+
+
         clear = (ImageButton) findViewById(R.id.Clear);
         desc = (TextView) findViewById(R.id.item);
         pre = (TextView) findViewById(R.id.itempre);
